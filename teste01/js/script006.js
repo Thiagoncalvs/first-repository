e1a527568434f7ef52b4c0807acddb139f2871d9
THIAGO S. GONÇALVES <thiago_gub@hotmail.com>
2021-05-14 07:01:00 -0300

    add test 14mai21

--- teste01/js/script006.js
+++ teste01/js/script006.js
@@ -1,7 +1,15 @@
-var vel = 61
+function verificando(){
+    var vel = window.document.querySelector('input#b')
+    var res = window.document.querySelector('div#resu')
+    var velo = Number(vel.value)
+    res.innerHTML = 'Sua velocidade é de ' + velo
+}
+
+
+/*var vel = 
 
 window.document.write("A velocidade é " + vel + 'km/h<br><br>')
 if(vel >= 60){
         window.document.write("Voce ultrapassou a velocidade permitida. Multado!<br><br>")
     }
-window.document.write('Não esqueça de usar o cinto de segurança')
+window.document.write('Não esqueça de usar o cinto de segurança')*/
---
+++ teste01/js/script007.js
@@ -0,0 +1,6 @@
+function calcular(){
+    var txtv = window.document.querySelector('input#txtvel')
+    var res = window.document.querySelector('div#res')
+    var vel = Number(txtv.value)
+    res.innerHTML = 'Sua velocidade atual é de ' + vel
+}
