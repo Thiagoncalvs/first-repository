0acfd379a39a72bc17986ab98c5c64a8e9bbab05
THIAGO S. GONÇALVES <thiago_gub@hotmail.com>
2021-05-13 22:38:30 -0300

    add plus

--- teste01/js/script006.js
+++ teste01/js/script006.js
@@ -1,6 +1,7 @@
 var vel = 61
 
-window.document.write("A velocidade é " + vel + 'km/h')
+window.document.write("A velocidade é " + vel + 'km/h<br><br>')
 if(vel >= 60){
-        window.document.write("Voce ultrapassou a velocidade permitida. Multado!")
-    }   
+        window.document.write("Voce ultrapassou a velocidade permitida. Multado!<br><br>")
+    }
+window.document.write('Não esqueça de usar o cinto de segurança')
