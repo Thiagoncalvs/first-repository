c800a1cce191009623acc51173f2f3f5ac72ce5d
THIAGO S. GONÇALVES <thiago_gub@hotmail.com>
2021-05-14 07:23:28 -0300

    add 007

--- teste01/js/script007.js
+++ teste01/js/script007.js
@@ -2,5 +2,11 @@ function calcular(){
     var txtv = window.document.querySelector('input#txtvel')
     var res = window.document.querySelector('div#res')
     var vel = Number(txtv.value)
-    res.innerHTML = 'Sua velocidade atual é de ' + vel
+    res.innerHTML = '<p>Sua velocidade atual é de ' + '<strong>' + vel + '</strong></p>'
+    if( vel > 60){
+        res.innerHTML += '<p>Sua velocidade é de ' + vel + ' , <strong>Multado!</strong></p>'
+    }else{
+        res.innerHTML += '<p>Sua velocidade é de ' + vel + ' , <strong>Pode seguir</strong></p>'
+    }
+    res.innerHTML += '<p>Dirija sempre com o cinto de segurança!</p>'
 }
