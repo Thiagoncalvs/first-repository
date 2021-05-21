5efee9112060d3f33ada2b52e368335c54b69ccf
THIAGO S. GONÇALVES <thiago_gub@hotmail.com>
2021-05-21 16:10:12 -0300

    addddd

--- teste01/js/TesteSlideScript.js
+++ teste01/js/TesteSlideScript.js
@@ -8,28 +8,14 @@ const nextBtn = document.querySelector('#nextBtn')
 
 // Counter
 
-let Counter = 4
+let Counter = 0
 const size = ImagensSlide[0].clientWidth
 
-ContainerSlide.style.transition = "transform 10s ease-in-out"
-Counter++
 ContainerSlide.style.transform = 'translateX(' + (-size * Counter) + 'px)'
-
-ContainerSlide.addEventListener('transitionend', ()=>{
-    console.log(ContainerSlide[Counter])
-    if(ImagensSlide[Counter].id === 'firstClone'){
-        ContainerSlide.style.transition = 'none'
-        Counter = ImagensSlide.length -Counter
-        ContainerSlide.style.transform = 'translateX(' + (-size * Counter) + 'px)'
-    }
-})
-
-
-
 
 
 // Button Listeners
-/*
+
 nextBtn.addEventListener('click', () => {
     if (Counter >= ImagensSlide.length -1) return
     ContainerSlide.style.transition = "transform 1s ease-in-out"
@@ -57,4 +43,3 @@ ContainerSlide.addEventListener('transitionend', ()=>{
         ContainerSlide.style.transform = 'translateX(' + (-size * Counter) + 'px)'
     }
 })
-*/
